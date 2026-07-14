3d6b1936b76d0539af3d8134637f398e6a707edb
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-07-14 21:17:25 +0200

    Add printTeacher function

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -30,3 +30,14 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName,
+  lastName
+): string => `${firstName.charAt(0)}. ${lastName}`;
+
+console.log(printTeacher('John', 'Doe'));
