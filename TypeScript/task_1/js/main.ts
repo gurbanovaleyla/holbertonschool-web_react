c9d5905a52130103b36ae1e689083edb04ff83f7
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-07-14 21:14:26 +0200

    Extend Teacher interface with Directors

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -9,10 +9,24 @@ interface Teacher {
 
 const teacher3: Teacher = {
   firstName: 'John',
-  lastName: 'Doe',
   fullTimeEmployee: false,
+  lastName: 'Doe',
   location: 'London',
   contract: false,
 };
 
 console.log(teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
