9fc03cf091f5c29d9d926fa4754deb01e08dbd9f
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-07-18 18:32:35 +0200

    Add StudentClass

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -41,3 +41,32 @@ const printTeacher: printTeacherFunction = (
 ): string => `${firstName.charAt(0)}. ${lastName}`;
 
 console.log(printTeacher('John', 'Doe'));
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(
+    private firstName: string,
+    private lastName: string
+  ) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass('John', 'Doe');
+
+console.log(student.displayName());
+console.log(student.workOnHomework());
